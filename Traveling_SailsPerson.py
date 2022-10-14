c848981a83b07f2f979e3816be22ac974f802009
stargazer1823 <noah.peterson@drake.edu>
2022-10-14 11:43:39 -0500

    Added traveling sailsPerson

---
+++ Traveling_SailsPerson.py
@@ -0,0 +1,17 @@
+def travil(n, W, P, minlength):
+    D = []
+    for i in range(n):
+        D.append({})
+    for i in range(2,n):
+        D[i]["0"] = W[i][1]
+    for k in range(1,n-2):
+            for k in range(1,n-2):
+                pass
+
+
+
+def subsets(lst):
+    oupt = []
+
+
+
